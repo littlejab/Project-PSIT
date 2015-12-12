8378f51caffb40d2374edc9120d0c942cfeae7e5
amoeba99 <kuro-neko@hotmail.co.th>
2015-12-12 23:33:10 +0900

    E20 and E85

--- e20-2013.py
+++ e20-2013.py
@@ -1,29 +1,34 @@
+"""Oil price graph"""
 import plotly.plotly as py
 import plotly.graph_objs as go
-py.sign_in('littlejab', 'yblima8sc3')
-chart_min = go.Bar(
-    x=['Jan 13', 'Feb 13', 'Mar 13', 'Apr 13', 'May 13', 'Jun 13', 'Jul 13', 'Aug 13', \
-    'Sep 13', 'Oct 13', 'Nov 13', 'Dec 13'],
-    y=[32.62, 34.33, 34.46, 32.86, 32.62, 33.70, 35.12, 34.47, 34.11, 33.62, 33.83, 34.79],
-    name='Min'
-)
+def e20_2013():
+    """E20 year 2013 price graph"""
+    py.sign_in("littlejab", "yblima8sc3")
+    chart_min = go.Bar(
+        x = ["Jan 13", "Feb 13", "Mar 13", "Apr 13", "May 13", "Jun 13", "Jul 13", "Aug 13", \
+            "Sep 13", "Oct 13", "Nov 13", "Dec 13"],
+        y = [32.62, 34.33, 34.46, 32.86, 32.62, 33.70, 35.12, 34.47, 34.11, 33.62, 33.83, 34.79],
+        name = "Min"
+    )
 
-chart_avg = go.Bar(
-    x=['Jan 13', 'Feb 13', 'Mar 13', 'Apr 13', 'May 13', 'Jun 13', 'Jul 13', 'Aug 13', \
-        'Sep 13', 'Oct 13', 'Nov 13', 'Dec 13'],
-    y=[32.62, 34.33, 34.46, 32.86, 32.62, 33.71, 35.12, 34.50, 34.34, 33.62, 33.83, 34.80],
-    name='Average'
-)
+    chart_avg = go.Bar(
+        x = ["Jan 13", "Feb 13", "Mar 13", "Apr 13", "May 13", "Jun 13", "Jul 13", "Aug 13", \
+           "Sep 13", "Oct 13", "Nov 13", "Dec 13"],
+        y = [32.62, 34.33, 34.46, 32.86, 32.62, 33.71, 35.12, 34.50, 34.34, 33.62, 33.83, 34.80],
+        name = "Average"
+    )
 
-chart_max = go.Bar(
-    x=['Jan 13', 'Feb 13', 'Mar 13', 'Apr 13', 'May 13', 'Jun 13', 'Jul 13', 'Aug 13', \
-        'Sep 13', 'Oct 13', 'Nov 13', 'Dec 13'],
-    y=[32.62, 34.33, 34.46, 32.86, 32.68, 33.82, 35.12, 34.73, 34.35, 33.63, 33.86, 34.86],
-    name='Max'
-)
-data = [chart_min, chart_avg, chart_max]
-layout = go.Layout(
-    barmode='group'
-)
-fig = go.Figure(data=data, layout=layout)
-plot_url = py.plot(fig, filename='E20 2013')
+    chart_max = go.Bar(
+        x = ["Jan 13", "Feb 13", "Mar 13", "Apr 13", "May 13", "Jun 13", "Jul 13", "Aug 13", \
+            "Sep 13", "Oct 13", "Nov 13", "Dec 13"],
+        y = [32.62, 34.33, 34.46, 32.86, 32.68, 33.82, 35.12, 34.73, 34.35, 33.63, 33.86, 34.86],
+        name = "Max"
+    )
+    data = [chart_min, chart_avg, chart_max]
+    layout = go.Layout(
+        barmode = "group"
+    )
+    fig = go.Figure(data=data, layout=layout)
+    plot_url = py.plot(fig, filename = "E20 2013")
+
+e20_2013()
--- e85-2015.py
+++ e85-2015.py
@@ -1,26 +1,31 @@
+"""Oil price graph"""
 import plotly.plotly as py
 import plotly.graph_objs as go
-py.sign_in('littlejab', 'yblima8sc3')
-chart_min = go.Bar(
-    x=['Jan 15', 'Feb 15', 'Mar 15', 'Apr 15', 'May 15', 'Jun 15'],
-    y=[21.98, 22.67, 23.48, 22.81, 23.19, 23.37],
-    name='Min'
-)
+def e85_2015():
+    """E85 year 2015 price graph"""
+    py.sign_in("littlejab", "yblima8sc3")
+    chart_min = go.Bar(
+        x = ["Jan 15", "Feb 15", "Mar 15", "Apr 15", "May 15", "Jun 15"],
+        y = [21.98, 22.67, 23.48, 22.81, 23.19, 23.37],
+        name = "Min"
+    )
 
-chart_avg = go.Bar(
-    x=['Jan 15', 'Feb 15', 'Mar 15', 'Apr 15', 'May 15', 'Jun 15'],
-    y=[21.98, 22.67, 23.48, 22.81, 23.19, 23.37],
-    name='Average'
-)
+    chart_avg = go.Bar(
+        x = ["Jan 15", "Feb 15", "Mar 15", "Apr 15", "May 15", "Jun 15"],
+        y = [21.98, 22.67, 23.48, 22.81, 23.19, 23.37],
+        name = "Average"
+    )
 
-chart_max = go.Bar(
-    x=['Jan 15', 'Feb 15', 'Mar 15', 'Apr 15', 'May 15', 'Jun 15'],
-    y=[21.98, 22.67, 23.48, 22.81, 23.19, 23.37],
-    name='Max'
-)
-data = [chart_min, chart_avg, chart_max]
-layout = go.Layout(
-    barmode='group'
-)
-fig = go.Figure(data=data, layout=layout)
-plot_url = py.plot(fig, filename='E85 2015')
+    chart_max = go.Bar(
+        x = ["Jan 15", "Feb 15", "Mar 15", "Apr 15", "May 15", "Jun 15"],
+        y = [21.98, 22.67, 23.48, 22.81, 23.19, 23.37],
+        name = "Max"
+    )
+    data = [chart_min, chart_avg, chart_max]
+    layout = go.Layout(
+        barmode = "group"
+    )
+    fig = go.Figure(data=data, layout=layout)
+    plot_url = py.plot(fig, filename="E85 2015")
+
+e85_2015()
